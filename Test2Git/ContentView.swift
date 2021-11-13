bb80b2e6ab825e7dcf01fe83b3d70b9b686e14b8
alonhi <93863138+alonhi@users.noreply.github.com>
2021-11-13 12:11:05 +0200

    Initial Commit

---
+++ Test2Git/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test2Git
+//
+//  Created by Andrii Cheberiako on 13.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
